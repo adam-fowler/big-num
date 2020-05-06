fa9c362dd6cede0e3aa8f23264e03b8d2b3fa230
Adam Fowler <adamfowler71@gmail.com>
2020-05-06 17:41:50 +0100

    Remove unnecessary files

--- Sources/CBigNumBoringSSL/include/CBigNumBoringSSL.h
+++ Sources/CBigNumBoringSSL/include/CBigNumBoringSSL.h
@@ -1,8 +1,15 @@
 #ifndef C_BIGNUM_BORINGSSL_H
 #define C_BIGNUM_BORINGSSL_H
 
-#include "CBigNumBoringSSL_base.h"
+#include "CBigNumBoringSSL_aes.h"
 #include "CBigNumBoringSSL_bio.h"
 #include "CBigNumBoringSSL_bn.h"
+#include "CBigNumBoringSSL_cipher.h"
+#include "CBigNumBoringSSL_cpu.h"
+#include "CBigNumBoringSSL_crypto.h"
+#include "CBigNumBoringSSL_bytestring.h"
+#include "CBigNumBoringSSL_err.h"
+#include "CBigNumBoringSSL_rand.h"
+
 
 #endif  // C_BIGNUM_BORINGSSL_H
